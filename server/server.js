9d7e303aac4aa07582d321f9b667cdc3d1add5f8
tu6619 <tu6619@my.bristol.ac.uk>
2016-05-17 16:46:45 +0100

    server set up

---
+++ server/server.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const Hapi = require('hapi')
+const Inert = require('inert')
+const Vision = require('vision')
+const Handlebars = require('Handlebars')
+
+
+
+
+const server = new Hapi.Server()
+
+const plugins = [Inert, Vision]
+
+
+server.connection({
+    port: process.env.PORT || 8000
+})
+
+server.register(plugins, function(err) {
+  if(err){
+    console.log('err--->', err)
+  }
+})
+
+
+module.exports = server;
