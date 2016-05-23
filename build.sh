45286b96e49f8e9af8d4f6f98f10e98c4e2430e5
tu6619 <tu6619@my.bristol.ac.uk>
2016-05-23 11:41:42 +0100

    try absolute path to prevent crashing build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 export GEM_HOME=$PWD/gems
 mkdir -p $GEM_HOME
 gem install sass
-$GEM_HOME/bin/sass sass/main.scss public/css/main.css
+$GEM_HOME/bin/sass $PWD/sass/main.scss $PWD/public/css/main.css
