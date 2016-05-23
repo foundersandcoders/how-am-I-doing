85c512408b5b44c1d4227c63b2bbc5ac9786c02c
Elias <elias0789@gmail.com>
2016-05-23 16:35:28 +0100

    Define default cookie properties for the JWT token. isSecure is environment variable to allow secure cookies on heroku, but insecure cookies on localhost

--- server/plugins/auth/index.js
+++ server/plugins/auth/index.js
@@ -5,13 +5,24 @@ const validate = require('./validate.js')
 exports.register = (server, options, next) => {
   const validator = validate(server)
 
-  server.auth.strategy('strategy-jwt', 'jwt',
-  { key: process.env.JWT_KEY,
+  server.auth.strategy('strategy-jwt', 'jwt', {
+    key: process.env.JWT_KEY,
     validateFunc: validator,
     verifyOptions: { algorithms: ['HS256'] }
   })
 
   server.auth.default('strategy-jwt')
+
+  server.state('token', {
+    ttl: null, // Session cookie (deleted when browser closed)
+    isSecure: process.env.ENV_PRODUCTION === 'true',
+    isHttpOnly: true,
+    encoding: 'none',
+    clearInvalid: true,
+    strictHeader: true,
+    path: '/'
+  })
+
   next()
 }
 
