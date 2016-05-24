03dc28a26ef7667c9a4302eeb75fa428b4436020
Elias <elias0789@gmail.com>
2016-05-24 14:55:57 +0100

    Make id's unique and add long category name

--- server/models/category.js
+++ server/models/category.js
@@ -2,8 +2,9 @@
 
 module.exports = (schema) => {
   return schema.define('category', {
-    cat_id: { type: schema.Number },
-    cat_name: { type: schema.String }
+    cat_id: { type: schema.Number, unique: true },
+    cat_name: { type: schema.String },
+    cat_long_name: { type: schema.String },
   },
   { primaryKeys: ['cat_id'] })
 }
--- server/models/question.js
+++ server/models/question.js
@@ -2,7 +2,7 @@
 
 module.exports = (schema) => {
   return schema.define('question', {
-    question_id: { type: schema.Number },
+    question_id: { type: schema.Number, unique: true },
     question_text: { type: schema.String }
   },
   { primaryKeys: ['question_id'] })
