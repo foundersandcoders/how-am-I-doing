7870f2d1908c568accd4e45a2391c4b67609afd9
Elias <elias0789@gmail.com>
2016-05-24 16:40:17 +0100

    Refer to extracted functionality instead of duplicating code

--- assets/js/db/migrate.js
+++ assets/js/db/migrate.js
@@ -5,23 +5,9 @@
 
 require('env2')('./config.env')
 
-const Caminte = require('caminte')
-const Schema = new Caminte.Schema(process.env.DB_DRIVER, {
-  password: process.env.DB_PASSWORD,
-  username: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  pool: true,
-  database: process.env.DB_NUMBER
-})
+const Schema = require('../../../server/db/schema.js')()
 
-const Question = require('../../../server/models/question.js')(Schema)
-const Questionnaire = require('../../../server/models/questionnaire.js')(Schema)
-const Category = require('../../../server/models/category.js')(Schema)
-
-Category.hasMany(Question, { as: 'question', foreignKey: 'cat_id' })
-Question.belongsTo(Category, { as: 'category', foreignKey: 'question_id' })
-Questionnaire.hasMany(Category, { as: 'category', foreignKey: 'questionnaire_id' })
+require('../../../server/db/relations.js')(Schema)
 
 const raw = {
   questions: require('../../../data.questions.json'),
@@ -31,7 +17,7 @@ const raw = {
 console.log('Categories: -----------------')
 
 const categories = raw.categories.map((cat) => {
-  const c = new Category()
+  const c = new Schema.models.Category()
   c.cat_id = cat.cat_id
   c.cat_name = cat.cat_name
   c.cat_long_name = cat.cat_long_name
