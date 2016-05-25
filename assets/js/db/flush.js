24a41042c652db513b7a65476f7a769d2da824c5
Elias <elias0789@gmail.com>
2016-05-25 12:53:48 +0100

    Simplify and fix flush

--- assets/js/db/flush.js
+++ assets/js/db/flush.js
@@ -5,89 +5,14 @@
 
 require('env2')('./config.env')
 
-const Caminte = require('caminte')
-const Schema = new Caminte.Schema(process.env.DB_DRIVER, {
-  password: process.env.DB_PASSWORD,
-  username: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  pool: true,
-  database: +process.env.DB_NUMBER
-})
+const Schema = require('../../../server/db/schema.js')()
 
-// const User = require('../../../server/models/user.js')(Schema)
-// const Question = require('../../../server/models/question.js')(Schema)
-// const Questionnaire = require('../../../server/models/questionnaire.js')(Schema)
-// const Category = require('../../../server/models/category.js')(Schema)
+const s = Schema.settings
 
-// const destroy = {
-//   category: () => {
-//     return new Promise((resolve, reject) => {
-//       Category.destroyAll((err) => {
-//         if (err)
-//           return reject(err)
-//
-//         console.log('Destroyed all categories')
-//         resolve()
-//       })
-//     })
-//   },
-//
-//   question: () => {
-//     return new Promise((resolve, reject) => {
-//       Question.destroyAll((err) => {
-//         if (err)
-//           return reject(err)
-//
-//         console.log('Destroyed all questions')
-//         resolve()
-//       })
-//     })
-//   },
-//
-//   questionnaire: () => {
-//     return new Promise((resolve, reject) => {
-//       Questionnaire.destroyAll((err) => {
-//         if (err)
-//           return reject(err)
-//
-//         console.log('Destroyed all questionnaires')
-//         resolve()
-//       })
-//     })
-//   },
-//
-//   user: () => {
-//     return new Promise((resolve, reject) => {
-//       User.destroyAll((err) => {
-//         if (err)
-//           return reject(err)
-//
-//         console.log('Destroyed all users')
-//         resolve()
-//       })
-//     })
-//   },
-// }
-//
-// if (process.argv.slice(2).indexOf('all') > -1) {
-//
-//   Promise.all(Object.keys(destroy).map((key) => destroy[key]()))
-//     .then(() => {
-//       Schema.client.quit()
-//     })
-//     .catch((err) => console.log('Error', err))
-//
-// } else {
-//
-//   Promise.all(process.argv.slice(2).map((arg) => {
-//     return destroy[arg] ? destroy[arg]() : Promise.resolve(null)
-//   }))
-//     .then(() => Schema.client.quit())
-//     .catch((err) => console.log('Error', err))
-//
-// }
+console.log('Dropping all tables from ', s.host + ':' + s.port + '/' + s.database)
 
-Schema.client.flushdb(() => {
-  Schema.client.quit()
+Schema.adapter.dropAllTables(() => {
+  console.log('Tables dropped successfully')
+  console.log('Disconnecting')
+  Schema.client.end()
 })
