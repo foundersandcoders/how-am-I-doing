61b7c873d9695ddae45b880204670d6b948dda9e
tu6619 <tu6619@my.bristol.ac.uk>
2016-05-25 19:13:42 +0100

    adapted to a more testable format - using a module pattern now

--- public/js/bar-chart.js
+++ public/js/bar-chart.js
@@ -30,86 +30,90 @@ const dummyData = [
 ]
 
 
-const qAnswers = dummyData.map((el) => {
-  return el.questionnaire_answers
-})
-
-const formatDate = d3.time.format('%a %d/%m/%Y')
-
-const qDates = dummyData.map((el) => {
-  return new Date(el.questionnaire_date)
-})
-
-const sum = qAnswers.map((el) => {
-  return el.reduce((a, b) => {
-    return a + b
+const createBarChart = (data) => {
+  const qAnswers = data.map((el) => {
+    return el.questionnaire_answers
   })
-})
 
-const createData = (dateArr, sumArr) => {
-  const a = dateArr.map((date) => {
-    return { date }
-  })
-  sumArr.forEach((val, i) => {
-    a[i].val = val
-  })
-  return a
-}
+  const formatDate = d3.time.format('%a %d/%m/%Y')
 
-const createBarChart = () => {
+  const qDates = data.map((el) => {
+    return new Date(el.questionnaire_date)
+  })
+
+  const sum = qAnswers.map((el) => {
+    return el.reduce((a, b) => {
+      return a + b
+    })
+  })
+
+  const createData = (dateArr, sumArr) => {
+    const a = dateArr.map((date) => {
+      return { date }
+    })
+    sumArr.forEach((val, i) => {
+      a[i].val = val
+    })
+    return a
+  }
+
   const margin = { top: 20, right: 30, bottom: 30, left: 40 },
     width = window.innerWidth - margin.left - margin.right,
     height = window.innerHeight / 2 - margin.top - margin.bottom
 
-  const x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], 0.1)
+  const inFunc = {}
 
-  const y = d3.scale.linear()
-      .range([height, 0])
+  inFunc.x = d3.scale.ordinal()
+  .rangeRoundBands([0, width], 0.1)
+  .domain(qDates)
 
-  const xAxis = d3.svg.axis()
-      .scale(x)
-      .orient('bottom')
-      .tickFormat((d) => formatDate(new Date(d)))
-      .ticks(4)
+  inFunc.y = d3.scale.linear()
+  .range([height, 0])
+  .domain([0, d3.max(sum)])
 
-  const yAxis = d3.svg.axis()
-      .scale(y)
-      .orient('left')
+  inFunc.render = () => {
+    const xAxis = d3.svg.axis()
+    .scale(inFunc.x)
+    .orient('bottom')
+    .tickFormat((d) => formatDate(new Date(d)))
+    .ticks(4)
 
-  const chart = d3.select('.chart')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    const yAxis = d3.svg.axis()
+    .scale(inFunc.y)
+    .orient('left')
 
+    const chart = d3.select('.chart')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-  x.domain(qDates)
-  y.domain([0, d3.max(sum)])
+    chart.append('g')
+    .attr('class', 'bax axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
 
-  chart.append('g')
-        .attr('class', 'bax axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis)
+    chart.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .text('Total')
 
-  chart.append('g')
-        .attr('class', 'y axis')
-        .call(yAxis)
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '.71em')
-        .style('text-anchor', 'end')
-        .text('Total')
+    chart.selectAll('.chart')
+    .data(createData(qDates, sum))
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d) => {return inFunc.x(d.date)})
+    .attr('y', (d) => {return inFunc.y(d.val)})
+    .attr('height', (d) => {return height - inFunc.y(d.val)})
+    .attr('width', inFunc.x.rangeBand())
 
-  chart.selectAll('.chart')
-        .data(createData(qDates, sum))
-        .enter().append('rect')
-        .attr('class', 'bar')
-        .attr('x', (d) => {return x(d.date)})
-        .attr('y', (d) => {return y(d.val)})
-        .attr('height', (d) => {return height - y(d.val)})
-        .attr('width', x.rangeBand())
+  }
+  return inFunc
 }
 
 const createLineChart = () => {
@@ -168,7 +172,7 @@ const createLineChart = () => {
         .attr('d', line(data))
 }
 
-createBarChart()
+createBarChart().render()
 
 let isDisplayingBar = true
 
