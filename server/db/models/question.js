290c86712efa334c00ff2c523191b651c4410e3e
Elias <elias0789@gmail.com>
2016-05-29 19:07:58 +0100

    question_id is a redundant field since unique id's are automatically included

--- server/db/models/question.js
+++ server/db/models/question.js
@@ -2,8 +2,7 @@
 
 module.exports = (schema) => {
   return schema.define('Question', {
-    question_id: { type: schema.Number, unique: true, index: true },
+    question_id: { type: schema.Number, unique: true },
     question_text: { type: schema.String }
-  },
-  { primaryKeys: ['question_id'] })
+  })
 }
