ac3fe6f43a5dde5dd9272f6dfccd2d9165443b74
tu6619 <tu6619@my.bristol.ac.uk>
2016-05-31 13:01:34 +0100

    share.js now sends email to recipient using mailgun-js node module

--- server/routes/share.js
+++ server/routes/share.js
@@ -1,11 +1,26 @@
 'use strict'
 
+// const http = require('https')
+const mailgun = require('mailgun-js')({
+  apiKey: process.env.API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+})
+
 exports.register = function (server, options, next) {
 
   server.route({
     method: 'GET',
     path: '/share',
     handler: (request, reply) => {
+      const data = {
+        from: 'Excited User <me@samples.mailgun.org>',
+        to: 'tom@foundersandcoders.com',
+        subject: 'Hello',
+        text: 'Testing some Mailgun awesomness!'
+      }
+      mailgun.messages().send(data, (error, body) => {
+        console.log(body)
+      })
       reply.view('share')
     }
   })
