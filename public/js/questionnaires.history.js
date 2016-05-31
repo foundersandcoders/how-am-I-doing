576939b33e6e2ff29038041fc516fb5339ac8e82
tu6619 <tu6619@my.bristol.ac.uk>
2016-05-31 18:00:56 +0100

    questionnaire history now contains one button per card which shares results with a clinician

--- public/js/questionnaires.history.js
+++ public/js/questionnaires.history.js
@@ -1,11 +1,27 @@
 /* global $ */
 'use strict'
 
+const xhttp = new XMLHttpRequest()
+
 ;(function () {
   $('.history').each((i, el) => {
     $(el).click((e) => {
-      const QUID = e.target.closest('.history').id
-      location.href = '/questionnaires/' + QUID + '/summary'
+      if (!e.target.disabled && e.target.classList.contains('share')) {
+        xhttp.onreadystatechange = function () {
+          if (xhttp.readyState === 4 && xhttp.status === 200
+            && JSON.parse(xhttp.responseText).success === true) {
+            console.log('Done')
+            e.target.classList.add('disabled')
+            e.target.disabled = true
+            e.target.innerHTML = 'Sent'
+          }
+        }
+        xhttp.open('GET', '/share/' + e.target.closest('.history').id)
+        xhttp.send()
+      } else {
+        const QUID = e.target.closest('.history').id
+        location.href = '/questionnaires/' + QUID + '/summary'
+      }
     })
   })
 })()
