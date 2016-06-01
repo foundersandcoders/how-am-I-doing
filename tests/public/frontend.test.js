1fac678fd01669d853b29368b7cead98fa37365a
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-01 18:29:53 +0100

    frontend tests broken

--- tests/public/frontend.test.js
+++ tests/public/frontend.test.js
@@ -5,51 +5,202 @@ let c
 
 const testData = [
   {
-    questionnaire_id: 1,
-    questionnaire_date: 1462111758000,
-    questionnaire_answers: [
-      0, 1, 2, 1, 3, 0, 0, 0, 1, 1
-    ]
-  }, {
-    questionnaire_id: 2,
-    questionnaire_date: 1462630158000,
-    questionnaire_answers: [
-      1, 1, 3, 0, 2, 1, 0, 0, 1, 1
-    ]
-  }, {
-    questionnaire_id: 3,
-    questionnaire_date: 1463321358000,
-    questionnaire_answers: [
-      0, 2, 2, 0, 1, 1, 0, 0, 0, 1
-    ]
-  }, {
-    questionnaire_id: 4,
-    questionnaire_date: 1463839758000,
-    questionnaire_answers: [
-      0, 1, 1, 0, 2, 1, 0, 0, 0, 1
-    ]
+    id: 1,
+    date: 1464688813000,
+    answers: [
+      {
+        answer: 1,
+        question_id: 4,
+        id: 1,
+        questionnaire_id: 1
+      },
+      {
+        answer: 1,
+        question_id: 7,
+        id: 2,
+        questionnaire_id: 1
+      },
+      {
+        answer: 2,
+        question_id: 8,
+        id: 3,
+        questionnaire_id: 1
+      },
+      {
+        answer: 3,
+        question_id: 12,
+        id: 4,
+        questionnaire_id: 1
+      },
+      {
+        answer: 1,
+        question_id: 20,
+        id: 5,
+        questionnaire_id: 1
+      },
+      {
+        answer: 0,
+        question_id: 30,
+        id: 6,
+        questionnaire_id: 1
+      },
+      {
+        answer: 2,
+        question_id: 32,
+        id: 7,
+        questionnaire_id: 1
+      },
+      {
+        answer: 0,
+        question_id: 38,
+        id: 8,
+        questionnaire_id: 1
+      },
+      {
+        answer: 2,
+        question_id: 43,
+        id: 9,
+        questionnaire_id: 1
+      }
+    ],
+    score: 12
+  },
+  {
+    id: 2,
+    date: 1464780664000,
+    answers: [
+      {
+        answer: 1,
+        question_id: 3,
+        id: 10,
+        questionnaire_id: 2
+      },
+      {
+        answer: 3,
+        question_id: 14,
+        id: 11,
+        questionnaire_id: 2
+      },
+      {
+        answer: 0,
+        question_id: 24,
+        id: 12,
+        questionnaire_id: 2
+      },
+      {
+        answer: 3,
+        question_id: 26,
+        id: 13,
+        questionnaire_id: 2
+      },
+      {
+        answer: 1,
+        question_id: 28,
+        id: 14,
+        questionnaire_id: 2
+      },
+      {
+        answer: 2,
+        question_id: 34,
+        id: 15,
+        questionnaire_id: 2
+      },
+      {
+        answer: 0,
+        question_id: 36,
+        id: 16,
+        questionnaire_id: 2
+      },
+      {
+        answer: 3,
+        question_id: 39,
+        id: 17,
+        questionnaire_id: 2
+      },
+      {
+        answer: 0,
+        question_id: 41,
+        id: 18,
+        questionnaire_id: 2
+      }
+    ],
+    score: 13
+  },
+  {
+    id: 3,
+    date: 1464780705000,
+    answers: [
+      {
+        answer: 0,
+        question_id: 5,
+        id: 19,
+        questionnaire_id: 3
+      },
+      {
+        answer: 0,
+        question_id: 9,
+        id: 20,
+        questionnaire_id: 3
+      },
+      {
+        answer: 1,
+        question_id: 17,
+        id: 21,
+        questionnaire_id: 3
+      },
+      {
+        answer: 2,
+        question_id: 18,
+        id: 22,
+        questionnaire_id: 3
+      },
+      {
+        answer: 3,
+        question_id: 33,
+        id: 23,
+        questionnaire_id: 3
+      },
+      {
+        answer: 0,
+        question_id: 45,
+        id: 24,
+        questionnaire_id: 3
+      },
+      {
+        answer: 1,
+        question_id: 46,
+        id: 25,
+        questionnaire_id: 3
+      }
+    ],
+    score: 7
   }
 ]
 
 QUnit.module('bar chart', {
 
   beforeEach: function () {
-    d3.selectAll('#target').append('svg').attr('class', 'chart')
     c = createBarChart(testData)
     c.render()
   },
   afterEach: function () {
-    d3.selectAll('.chart').remove()    // clean up after each test
+    d3.select('.chart').selectAll('*').remove()    // clean up after each test
+    return true
   }
 })
 
 QUnit.test('the svg should be created', (assert) => {
   function getChart () {
-    return d3.select('.chart')
+    return d3.select('.chart svg')
   }
   assert.notEqual(getChart(), null, 'svg definitely exists!')
-  assert.equal(getChart().attr('width'), window.innerWidth, 'svg width is as expected')
-  assert.equal(getChart().attr('height'), window.innerHeight / 2, 'svg height is as expected')
+
+  console.log(getChart())
+  assert.equal(getChart().attr('width'),
+  Math.min(window.innerWidth, 800), 'svg width is as expected')
+
+  assert.equal(getChart().attr('height'),
+  Math.min(window.innerHeight, 800) / 2, 'svg height is as expected')
 })
 
 QUnit.test('bars can be created', (assert) => {
