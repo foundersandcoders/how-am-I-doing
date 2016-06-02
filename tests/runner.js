94a7a75b44bcb74f6229d71f70b3cd7c308f74bf
tu6619 <tu6619@my.bristol.ac.uk>
2016-06-02 17:57:08 +0100

    test runner requires new user-flow test file

--- tests/runner.js
+++ tests/runner.js
@@ -2,5 +2,6 @@
 
 require('env2')('./config.env')
 
-require('./server/db/schema.test.js')
-require('./server/server.test.js')
+// require('./server/db/schema.test.js')
+// require('./server/server.test.js')
+require('./server/user-flow-1.test.js')
