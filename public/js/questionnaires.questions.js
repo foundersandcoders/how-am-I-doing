277414055e8b0462a311325d302cd84d34363e27
Elias <elias0789@gmail.com>
2016-06-07 18:08:10 +0100

    Reduce code duplication

--- public/js/questionnaires.questions.js
+++ public/js/questionnaires.questions.js
@@ -13,30 +13,18 @@
     $('html, body').animate({ scrollTop: target.offsetTop - padding }, 1000)
   }
 
-  function scrollNext (currentNode) {
-    var nextNodeId = +currentNode + 1
-    scrollToId('prev-' + nextNodeId)
+  function eachHandler (direction) {
+    return function (i, element) {
+      $(element).click(function (e) {
+        e.preventDefault()
+        var current = $(e.target).closest('.arrow').attr('id').slice(5)
+        var nextNodeId = +current + direction * 1
+        if (nextNodeId > -1)
+          scrollToId('prev-' + nextNodeId)
+      })
+    }
   }
 
-  function scrollPrev (currentNode) {
-    var nextNodeId = +currentNode - 1
-    if (nextNodeId > -1)
-      scrollToId('prev-' + nextNodeId)
-  }
-
-  $('[id^=prev-]').each(function (i, element) {
-    $(element).click(function (e) {
-      e.preventDefault()
-      var current = $(e.target).closest('.arrow').attr('id').slice(5)
-      scrollPrev(current)
-    })
-  })
-
-  $('[id^=next-]').each(function (i, element) {
-    $(element).click(function (e) {
-      e.preventDefault()
-      var current = $(e.target).closest('.arrow').attr('id').slice(5)
-      scrollNext(current)
-    })
-  })
+  $('[id^=prev-]').each(eachHandler(-1))
+  $('[id^=next-]').each(eachHandler(+1))
 })()
