8fed592349d9eeac01bdf701be53a90319841215
Elias <elias0789@gmail.com>
2016-06-08 18:37:55 +0100

    Ensure integers

--- tests/helpers/utils.js
+++ tests/helpers/utils.js
@@ -4,4 +4,4 @@ exports.randstr = (len) => {
   return (Date.now() * Math.random()).toString(36).replace('.', '').slice(0, len)
 }
 
-exports.randnum = (min, max) => (max - min) * Math.random() + min
+exports.randnum = (min, max) => Math.round((max - min) * Math.random() + min)
